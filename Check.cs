8a9c144758e894e1fb8866fced20bd9392b36788
Junior Oliveira <junioroliveira@hotmail.com>
2022-08-22 22:35:17 -0300

    Adicionar arquivos de projeto.

---
+++ Check.cs
@@ -0,0 +1,113 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validacao
+{
+    public class Check
+    {
+        #region VERIFICAR CONEXÃO
+        //Método da API
+        [DllImport("wininet.dll")]
+        private extern static Boolean InternetGetConnectedState(out int Description, int ReservedValue);
+
+        // Um método que verifica se esta conectado
+        public static Boolean IsConnected()
+        {
+            int Description;
+            return InternetGetConnectedState(out Description, 0);
+        }
+        #endregion
+
+        public static Tuple<Retorno, string> Update(string myPrograma = null, string currentVersion = null)
+        {
+            string path = Directory.GetCurrentDirectory() + "\\pooBin.dll";
+            string date2 = null;
+
+            if (IsConnected())
+            {
+                #region CARREGAR DADOS
+
+                DateTime hoje = DateTime.Now;
+
+
+                var Cliente = new WebClient();
+                var json = Cliente.DownloadString("https://pastebin.com/raw/bYcMxLsD");
+
+                Programas.programasList = new();
+                Programas.getArquivoName = new();
+                Programas.getLinkDownload = new();
+                Programas.getValidade = new();
+                Programas.getVersao = new();
+
+                Programas.programasList = JsonConvert.DeserializeObject<List<Programas>>(json);
+
+                var getVersao = Programas.getVersao[myPrograma];
+                var getValidade = Programas.getValidade[myPrograma];
+                var getLink = Programas.getLinkDownload[myPrograma] + ";" + Programas.getArquivoName[myPrograma];
+
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+
+                if (getVersao == currentVersion)
+                {
+                    var resul = DateTime.Compare(hoje, Convert.ToDateTime(getValidade));
+                    if (resul >= 0)
+                    {
+                        return new Tuple<Retorno, string>(Retorno.LicencaVencida, string.Empty);
+                    }
+                    else
+                    {
+                        return new Tuple<Retorno, string>(Retorno.Liberado, string.Empty);
+                    }
+                }
+                else
+                {
+                    return new Tuple<Retorno, string>(Retorno.VersaoDesatualizada, getLink);
+                }
+
+                #endregion
+            }
+            else
+            {
+                if (File.Exists(path))
+                {
+                    string text = System.IO.File.ReadAllText(path);
+                    date2 = text;
+                }
+                else
+                {
+                    File.Create(path).Close();
+
+                    using (StreamWriter sw = File.AppendText(path))
+                    {
+                        DateTime date = DateTime.Now.AddDays(5);
+                        sw.WriteAsync(date.ToShortDateString());
+                    }
+
+                    string text = System.IO.File.ReadAllText(path);
+                    date2 = text;
+                }
+
+                return new Tuple<Retorno, string>(Retorno.Liberado, string.Empty);
+            }
+            return new Tuple<Retorno, string>(Retorno.Liberado, string.Empty);
+        }
+    }
+
+
+    public enum Retorno
+    {
+        Liberado,
+        LicencaVencida,
+        VersaoDesatualizada
+    }
+}
---
+++ Programas.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validacao
+{
+    internal class Programas
+    {
+        public static List<Programas> programasList = new();
+        public static Dictionary<string, string> getValidade = new Dictionary<string, string>();
+        public static Dictionary<string, string> getVersao = new Dictionary<string, string>();
+        public static Dictionary<string, string> getLinkDownload = new Dictionary<string, string>();
+        public static Dictionary<string, string> getArquivoName = new Dictionary<string, string>();
+
+        private string _programa { get; set; }
+        public string Programa
+        {
+            get { return _programa; }
+            set { _programa = value; }
+        }
+
+
+        private string _versao;
+        public string Versao
+        {
+            get { return _versao; }
+            set { _versao = value; }
+        }
+
+        private string _chave;
+        public string Chave
+        {
+            get { return _chave; }
+            set { _chave = value; }
+        }
+
+        private bool _status;
+        public bool Status
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
+
+        private string _validade;
+        public string Validade
+        {
+            get { return _validade; }
+            set { _validade = value; }
+        }
+
+        private string _linkDownload;
+        public string LinkDownload
+        {
+            get { return _linkDownload; }
+            set { _linkDownload = value; }
+        }
+
+        private string _fileName;
+        public string FileName
+        {
+            get { return _fileName; }
+            set { _fileName = value; }
+        }
+
+        public Programas(string programa, string versao, string chave, bool status, string validade, string linkDownload, string fileName)
+        {
+            if (status)
+            {
+                Programa = programa;
+                Versao = versao;
+                Chave = chave;
+                Status = status;
+                Validade = validade;
+                LinkDownload = linkDownload;
+                FileName = fileName;
+
+                getValidade.Add(programa, validade);
+                getVersao.Add(programa, versao);
+                getLinkDownload.Add(programa, linkDownload);
+                getArquivoName.Add(programa, fileName);
+            }
+        }
+
+
+    }
+}
